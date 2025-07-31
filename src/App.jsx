9884a9ea55a686ab5d5dce1f66cb239e302ac76d
Giannina Miñano <g.isabella.md@gmail.com>
2025-07-31 20:54:36 -0300

    Refactor `Room` component: integrate new materials and selective bloom effect. Add new sections (`ShowcaseSection`, `Experience`, `FeatureCards`) and components (`NavBar`, `GlowCard`, `TitleHeader`, `AnimatedCounter`, `LogoShowcase`). Update dependencies with `react-countup`, `prettier` setup, and `eslint-config-prettier`. Enhance `Button` click behavior.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import Hero from './sections/Hero'
+import Hero from './sections/Hero';
+import ShowcaseSection from './sections/ShowcaseSection.jsx';
+import Navbar from './components/Navbar';
+import LogoShowcase from './sections/LogoShowcase';
+import FeatureCards from './sections/FeatureCards';
+import Experience from './sections/Experience';
 
 const App = () => {
-    return (
-        <>
-            <Hero />
-        </>
-    )
-}
-export default App
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <ShowcaseSection />
+      <LogoShowcase />
+      <FeatureCards />
+      <Experience />
+    </>
+  );
+};
+export default App;
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,54 +1,69 @@
-import React from 'react'
-import { words } from '../constants'
-import Button from '../components/Button'
-import HeroExperience from "../components/HeroModels/HeroExperience.jsx";
+import React from 'react';
+import { words } from '../constants';
+import Button from '../components/Button';
+import HeroExperience from '../components/HeroModels/HeroExperience.jsx';
+import { useGSAP } from '@gsap/react';
+import { gsap } from 'gsap';
+import AnimatedCounter from '../components/AnimatedCounter';
 
 const Hero = () => {
-    return (
-        <section id="hero" className="relative overflow-hidden">
-            <div className="absolute top-0 left-0 z-10">
-                <img src="/images/bg.png" alt="background" />
+  useGSAP(() => {
+    gsap.fromTo(
+      '.hero-text h1',
+      { opacity: 0, y: 50 },
+      { opacity: 1, y: 0, duration: 1.5, stagger: 0.2, ease: 'power2.inOut' }
+    );
+  });
+
+  return (
+    <section id="hero" className="relative overflow-hidden">
+      <div className="absolute top-0 left-0 z-10">
+        <img src="/images/bg.png" alt="background" />
+      </div>
+
+      <div className="hero-layout">
+        <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
+          <div className="flex flex-col gap-7">
+            <div className="hero-text">
+              <h1>
+                Shaping
+                <span className="slide">
+                  <span className="wrapper">
+                    {words.map((word) => (
+                      <span key={word.text} className="flex items-center md:gap-3 gap-1 pb-2">
+                        <img
+                          src={word.imgPath}
+                          alt={word.text}
+                          className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"
+                        />
+                        <span>{word.text}</span>
+                      </span>
+                    ))}
+                  </span>
+                </span>
+              </h1>
+              <h1>into Real Projects</h1>
+              <h1>that Deliver Results</h1>
             </div>
 
-            <div className="hero-layout">
-                <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
-                    <div className="flex flex-col gap-7">
-                        <div className="hero-text">
-                            <h1>Shaping
-                                <span className="slide">
-                                    <span className="wrapper">
-                                        {words.map((word ) => (
-                                            <span  key={word.text} className="flex items-center md:gap-3 gap-1 pb-2">
-                                                <img src={word.imgPath} alt={word.text} className="xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50"/>
-                                                <span>{word.text}</span>
-                                            </span>
-                                        ))}
+            <p className="md:text-xl relative z-10 text-white-50 pointer-events-none">
+              Hi, I'm <span className="text-white">Giannina</span>, a Frontend Developer based in
+              Argentina with a passion for crafting exceptional digital experiences.
+            </p>
 
-                                    </span>
-                                </span></h1>
-                            <h1>into Real Projects</h1>
-                            <h1>that Deliver Results</h1>
-                        </div>
+            <Button className="md:w-80 md:h-16 w-60 h-12" id="button" text="See my work" />
+          </div>
+        </header>
 
-                        <p className="md:text-xl relative z-10 text-white-50 pointer-events-none" >
-                            Hi, I'm <span className="text-white">Giannina</span>, a Frontend Developer based in Argentina with a passion for crafting exceptional digital experiences.
-                        </p>
+        <figure>
+          <div className="hero-3d-layout">
+            <HeroExperience />
+          </div>
+        </figure>
+      </div>
 
-                        <Button className="md:w-80 md:h-16 w-60 h-12" id="button" text="See my work" />
-                    </div>
-                </header>
-
-                <figure>
-                    <div className="hero-3d-layout">
-                        <HeroExperience/>
-                    </div>
-
-                </figure>
-
-
-            </div>
-
-        </section>
-    )
-}
-export default Hero
+      <AnimatedCounter />
+    </section>
+  );
+};
+export default Hero;
