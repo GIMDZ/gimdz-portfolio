e0a7a52a3f22ca51c765b8fc2e7d2adce4437581
Giannina Miñano <g.isabella.md@gmail.com>
2025-08-11 17:47:05 -0300

    Add `TechStack`, `Testimonials`, `Contact`, and `Footer` sections. Implement new components including `TechIconCardExperience`, `Computer`, and `ContactExperience`. Refactor file structure for better organization. Update dependencies with `@emailjs/browser`. Add email functionality and environment variable support. Update title in `index.html`.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Navbar from './components/Navbar';
 import LogoShowcase from './sections/LogoShowcase';
 import FeatureCards from './sections/FeatureCards';
 import Experience from './sections/Experience';
+import TechStack from './sections/TechStack';
+import Testimonials from './sections/Testimonials';
+import Contact from './sections/Contact';
+import Footer from './sections/Footer';
 
 const App = () => {
   return (
@@ -14,6 +18,10 @@ const App = () => {
       <LogoShowcase />
       <FeatureCards />
       <Experience />
+      <TechStack />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </>
   );
 };
